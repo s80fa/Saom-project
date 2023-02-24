fb374cedf71a148303ee21658088ed108c59cd18
Suraiya Shefa <shefa@Suraiyas-Air.lan>
2023-02-24 12:45:52 -0800

    first commit

---
+++ js/table.js
@@ -0,0 +1,23 @@
+
+var $table = $('#table')
+
+        $(function () {
+            $('#toolbar').find('select').change(function () {
+                $table.bootstrapTable('destroy').bootstrapTable({
+                    exportDataType: $(this).val(),
+                    exportTypes: ['json', 'xml', 'csv', 'txt', 'sql', 'excel', 'pdf'],
+                    columns: [
+                        {
+                            field: 'id',
+                            title: 'Username'
+                        }, {
+                            field: 'source',
+                            title: 'Database'
+                        }, {
+                            field: 'percentage',
+                            title: 'Similarity Score (%)'
+                        }
+                    ]
+                })
+            }).trigger('change')
+        })
